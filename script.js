0f9de01040a6fa802616cce754c0eafc19778c7b
A. Mambaus Sholihin <bauagg87@gmail.com>
2023-03-15 11:07:21 +0700

    baus

--- script.js
+++ script.js
@@ -1,36 +1,46 @@
 
 const btnSearch = document.querySelector(`#search-btn`)
 btnSearch.addEventListener('click', async function () {
-    const imputSearch = document.querySelector(`.form-control`)
-    const brita = await getBrita(imputSearch.value)
-    console.log(brita)
-    // updateUI(brita)
+
+    try {
+        const imputSearch = document.querySelector('.form-control')
+        const brita = await getBrita(imputSearch.value)
+        console.log(brita)
+        updateUI(brita)
+    }
+
+    catch (error) {
+        console.log(error)
+    }
+
+    finally {
+        loading()
 
 
+    }
 });
 
-function getBrita(keyword) {
-
-    fetch('https://newsapi.org/v2/everything?q=bitcoin&apiKey=f6f9b5ff08ed46d8ab32401be3684d00')
+function getBrita(getData) {
+    return fetch(`https://newsapi.org/v2/everything?q=${getData}&apiKey=f6f9b5ff08ed46d8ab32401be3684d00`)
         .then(data => data.json())
         .then(data => {
-            data + keyword
-            return console.log(data.articles)
-
+            return data.articles;
         })
 }
-getBrita()
 
 
 function updateUI(data) {
-    let card = '';
-    data.forEach(e => card + showCards(e))
-    const britaContainer = document.querySelector('.brita-container')
-    britaContainer.innerHTML = card;
+    let datas = [];
+    data.forEach(e => {
+        const card = showCards(e)
+        datas.push(card)
+    });
+
+    document.getElementsByClassName('row brita-container')[0].innerHTML = datas;
 }
 
 function showCards(m) {
-    return `<div class="col-md-4 my-5">
+    return `<div class="col-md-3 my-0">
                 <div class="card">
                     <img src="${m.urlToImage}">
                     <div class="card-body">
@@ -41,3 +51,21 @@ function showCards(m) {
                 </div>
             </div>`;
 }
+
+function loading() {
+    let lod = load()
+    document.getElementsByClassName('load')[0].innerHTML = lod;
+    console.log(lod)
+}
+
+function load() {
+    return `
+    <div class="text-center m-5">
+        <div class="spinner-border" role="status">
+            <span class="sr-only">Loading...</span>
+        </div>
+    </div>`
+}
+
+
+
